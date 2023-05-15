ca28ebad2baabd465e80533a964dcc893af943bb
Kevin <kevinkolyakov@gmail.com>
2023-05-15 11:39:38 -0400

    Started working on menu

--- core/src/com/git/game/GitOdyssey.java
+++ core/src/com/git/game/GitOdyssey.java
@@ -4,28 +4,32 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import jdk.tools.jmod.Main;
 
 public class GitOdyssey extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	MainMenu mainMenu = new MainMenu();
+
 	@Override
 	public void create () {
+		mainMenu.create();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+//		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		mainMenu.render();
+//		ScreenUtils.clear(1, 0, 0, 1);
+//		batch.begin();
+////		batch.draw(img, 0, 0);
+//		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+//		batch.dispose();
+//		img.dispose();
 	}
 }
