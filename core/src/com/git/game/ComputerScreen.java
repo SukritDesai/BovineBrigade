d1db0bbb832e70c6e2d559112c5e258d3552feb9
Kev1nKolyakov <kevin.kolyakov@student.tdsb.on.ca>
2023-05-24 14:09:34 -0400

    added squares

--- core/src/com/git/game/ComputerScreen.java
+++ core/src/com/git/game/ComputerScreen.java
@@ -30,8 +30,8 @@ public class ComputerScreen implements Screen {
     private SpriteBatch spriteBatch;
     private ShapeRenderer shapeRenderer;
     private TextField consoleTextField;
-    Label output;
-    Rectangle first;
+    Label output, firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel;
+    Rectangle first, second, third, fourth, fifth, connector1, connector2, connector3, connector4, connector5, outline;
 
 
     @Override
@@ -45,12 +45,60 @@ public class ComputerScreen implements Screen {
         Gdx.input.setInputProcessor(stage);
 
         // Create and position the rectangles in the left half of the screen
-        float rectangleWidth = SCREEN_WIDTH / 5f;
+        float rectangleWidth = SCREEN_WIDTH / 8f;
         float rectangleX = 1f;
-        float rectangleY = 1f;
-        first = new Rectangle(rectangleX, rectangleY, rectangleWidth, rectangleWidth, Color.RED);
+        float rectangleY = 330;
+        Label title = new Label("Visual Representation", gameSkin, "default");
+        firstLabel = new Label("", gameSkin, "default");
+        secondLabel = new Label("", gameSkin, "default");
+        thirdLabel = new Label("", gameSkin, "default");
+        fourthLabel = new Label("", gameSkin, "default");
+        fifthLabel = new Label("", gameSkin, "default");
+        title.setPosition(10, 445);
+        firstLabel.setPosition(30, 380);
+        secondLabel.setPosition(30, 255);
+        thirdLabel.setPosition(180, 255);
+        fourthLabel.setPosition(180, 130);
+        fifthLabel.setPosition(30, 90);
+        outline = new Rectangle(rectangleX-60, rectangleY-300, rectangleWidth+310, rectangleWidth+340, Color.BLACK);
+        connector1 = new Rectangle(rectangleX+65, rectangleY-50, 10, 50, Color.WHITE);
+        connector2 = new Rectangle(rectangleX+65, rectangleY-200, 10, 100, Color.WHITE);
+        connector3 = new Rectangle(rectangleX+110, rectangleY-80, 60, 10, Color.WHITE);
+        connector4 = new Rectangle(rectangleX+215, rectangleY-175, 10, 50, Color.WHITE);
+        connector5 = new Rectangle(rectangleX+120, rectangleY-230, 100, 10, Color.WHITE);
+        first = new Rectangle(rectangleX+20, rectangleY, rectangleWidth, rectangleWidth, Color.RED);
+        second = new Rectangle(rectangleX+20, rectangleY-125, rectangleWidth, rectangleWidth, Color.RED);
+        third = new Rectangle(rectangleX+170, rectangleY-125, rectangleWidth, rectangleWidth, Color.YELLOW);
+        fifth = new Rectangle(rectangleX+170, rectangleY-250, rectangleWidth, rectangleWidth, Color.YELLOW);
+        fourth = new Rectangle(rectangleX+20, rectangleY-290, rectangleWidth, rectangleWidth, Color.RED);
+        connector1.setTransparency(0f);
+        connector2.setTransparency(0f);
+        connector3.setTransparency(0f);
+        connector4.setTransparency(0f);
+        connector5.setTransparency(0f);
         first.setTransparency(0f);
+        second.setTransparency(0f);
+        third.setTransparency(0f);
+        fourth.setTransparency(0f);
+        fifth.setTransparency(0f);
+
+        stage.addActor(outline);
+        stage.addActor(connector1);
+        stage.addActor(connector2);
+        stage.addActor(connector3);
+        stage.addActor(connector4);
+        stage.addActor(connector5);
         stage.addActor(first);
+        stage.addActor(second);
+        stage.addActor(third);
+        stage.addActor(fourth);
+        stage.addActor(fifth);
+        stage.addActor(title);
+        stage.addActor(firstLabel);
+        stage.addActor(secondLabel);
+        stage.addActor(thirdLabel);
+        stage.addActor(fourthLabel);
+        stage.addActor(fifthLabel);
 
         // Create and position the instructions label in the top right
         Label instructionsLabel = new Label("Instructions:\nPlease clone the\nrepository", gameSkin, "default");
@@ -86,10 +134,24 @@ public class ComputerScreen implements Screen {
 
         // Check if the user presses enter in the console text field
         if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            if (consoleTextField.getText().equals("git clone")) {
+            if (consoleTextField.getText().trim().equals("git clone")) {
                 output.setColor(Color.GREEN);
                 output.setText("This is to be completed");
                 first.setTransparency(1f);
+                second.setTransparency(1f);
+                third.setTransparency(1f);
+                fourth.setTransparency(1f);
+                fifth.setTransparency(1f);
+                connector1.setTransparency(1f);
+                connector2.setTransparency(1f);
+                connector3.setTransparency(1f);
+                connector4.setTransparency(1f);
+                connector5.setTransparency(1f);
+                firstLabel.setText("9e78i");
+                secondLabel.setText("035cc");
+                thirdLabel.setText("e3475");
+                fourthLabel.setText("76d12");
+                fifthLabel.setText("i8fe5");
             } else {
                 output.setColor(Color.RED);
                 output.setText("Error, please try again");
