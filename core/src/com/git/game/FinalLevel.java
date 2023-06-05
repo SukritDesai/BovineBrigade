f017f9990f8fd7bce413b7a8447758f42b93c206
Sukrit Desai <sukritdesai@gmail.com>
2023-06-05 19:44:29 -0400

    Fixed Tileset naming

--- core/src/com/git/game/FinalLevel.java
+++ core/src/com/git/game/FinalLevel.java
@@ -9,16 +9,26 @@ package com.git.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class FinalLevel implements Screen {
+    private final OrthogonalTiledMapRenderer tiledMapRenderer;
     private Character character;
+    private OrthographicCamera camera;
 
     public FinalLevel() {
         character = new Character("character.png");
+        TiledMap map = new TmxMapLoader().load("GameMap.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
     }
 
     @Override
@@ -28,8 +38,14 @@ public class FinalLevel implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         character.update(delta);
         character.render();
+
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
     }
 
     @Override
