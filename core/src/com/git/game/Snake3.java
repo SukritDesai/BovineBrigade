5adcebb51a1ebb4101581e6cf15fbed9f0cda09c
Kev1nKolyakov <kevin.kolyakov@student.tdsb.on.ca>
2023-06-09 10:14:11 -0400

    Fix: message in Snake3

--- core/src/com/git/game/Snake3.java
+++ core/src/com/git/game/Snake3.java
@@ -154,7 +154,7 @@ public class Snake3 implements Screen {
         stage.addActor(fifthLabel);
 
         // Create and position the instructions label in the top right
-        instructionsLabel = new Label("\n\nInstructions:\nThe last commit made by a user was \nfaulty. Reset the commit.", gameSkin, "default");
+        instructionsLabel = new Label("\n\nInstructions:\nThe last commit made by a user was \nfaulty. Reset the commit to 9e78i.", gameSkin, "default");
         instructionsLabel.setPosition(SCREEN_WIDTH / 2f, SCREEN_HEIGHT - instructionsLabel.getHeight()+20);
         instructionsLabel.setFontScale(0.6f);
 
